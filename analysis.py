5e056e7022ed7997d38a41aa816935aab2726a3a
Bryan Cora <corabj@mail.uc.edu>
2023-10-15 13:05:05 -0400

    F1 and accuracy

--- analysis.py
+++ analysis.py
@@ -1,14 +1,50 @@
 import pandas as pd
-def F1(y_true: pd.Series, y_pred: pd.Series) -> float:
-    TP = ((y_true == 1) & (y_true == 1)).sum()
+import plotly.express as px
+def accuracy(y_true: pd.Series, y_pred: pd.Series) -> float:
+    """returns the accuracy of the model
+
+    Args:
+        y_true (pd.Series): treu lables
+        y_pred (pd.Series): predicted labels
+
+    Returns:
+        float: accuracy [0-100]
+    """
+    TP = ((y_true == 1) & (y_pred == 1)).sum()
+    TN =  ((y_true == 0) & (y_pred == 0)).sum()
+    return TP+TN/len(y_true)*100
+    
+def accuracy_plot(): 
     pass
 
+
+def F1(y_true: pd.Series, y_pred: pd.Series) -> float:
+    """calculate F1 score given true labels and predicted labels
+
+    Args:
+        y_true (pd.Series): true labels
+        y_pred (pd.Series): predicted labels
+
+    Returns:
+        float: f1 score [0-1] 1 being a perfect score
+    """
+    TP = ((y_true == 1) & (y_pred == 1)).sum()
+    FP =  ((y_true == 0) & (y_pred == 1)).sum()
+    FN =  ((y_true == 1) & (y_pred == 0)).sum()
+    
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    
+    f1 = 2*(precision*recall)/(precision+recall)
+    return f1
+
+
+
+#TODO: decide what the input should look like
 def F1_plot():
     pass
 
-
-def ROC():
-    pass
+# def 
 
 def ROC_plot():
     pass
