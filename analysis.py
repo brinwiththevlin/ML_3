51e29130ef34183778fafed4ab3da3f6f7a7e32c
Bryan Cora <corabj@mail.uc.edu>
2023-10-16 14:50:54 -0400

    adding black formatting

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ from typing import List
 
 plotly.io.renderers.default = "browser"
 
+
 def accuracy(y_true: pd.Series, y_pred: pd.Series) -> float:
     """returns the accuracy of the model
 
@@ -18,26 +19,31 @@ def accuracy(y_true: pd.Series, y_pred: pd.Series) -> float:
         float: accuracy [0-100]
     """
     TP = ((y_true == 1) & (y_pred == 1)).sum()
-    TN =  ((y_true == 0) & (y_pred == 0)).sum()
-    return TP+TN/len(y_true)*100
-    
-def accuracy_plot(accuracies: np.ndarray, model: str )->None: 
+    TN = ((y_true == 0) & (y_pred == 0)).sum()
+    return TP + TN / len(y_true) * 100
+
+
+def accuracy_plot(accuracies: np.ndarray, model: str) -> None:
     """creates accuracy plot against bin size
 
     Args:
         accuracies (np.ndarray): each row corresponds to one of 5 splits, each column is the bin size
         model (str): name of the model being analyzed
     """
-    bins = [5,10,15,20]
-    trace1 = go.Scatter(x=bins, y=accuracies[:,0], mode='lines', name='split 1')
-    trace2 = go.Scatter(x=bins, y=accuracies[:,1], mode='lines', name='split 2')
-    trace3 = go.Scatter(x=bins, y=accuracies[:,2], mode='lines', name='split 3')
-    trace4 = go.Scatter(x=bins, y=accuracies[:,3], mode='lines', name='split 4')
-    trace5 = go.Scatter(x=bins, y=accuracies[:,4], mode='lines', name='split 5')
+    bins = [5, 10, 15, 20]
+    trace1 = go.Scatter(x=bins, y=accuracies[:, 0], mode="lines", name="split 1")
+    trace2 = go.Scatter(x=bins, y=accuracies[:, 1], mode="lines", name="split 2")
+    trace3 = go.Scatter(x=bins, y=accuracies[:, 2], mode="lines", name="split 3")
+    trace4 = go.Scatter(x=bins, y=accuracies[:, 3], mode="lines", name="split 4")
+    trace5 = go.Scatter(x=bins, y=accuracies[:, 4], mode="lines", name="split 5")
     traces = [trace1, trace2, trace3, trace4, trace5]
-    
-    layout = go.Layout(title=f'accuracy against bin size for {model}', xaxis=dict(title='bin size'), yaxis=dict(title='accuracy'))
-    
+
+    layout = go.Layout(
+        title=f"accuracy against bin size for {model}",
+        xaxis=dict(title="bin size"),
+        yaxis=dict(title="accuracy"),
+    )
+
     fig = go.Figure(data=traces, layout=layout)
     fig.show()
 
@@ -53,33 +59,37 @@ def F1(y_true: pd.Series, y_pred: pd.Series) -> float:
         float: f1 score [0-1] 1 being a perfect score
     """
     TP = ((y_true == 1) & (y_pred == 1)).sum()
-    FP =  ((y_true == 0) & (y_pred == 1)).sum()
-    FN =  ((y_true == 1) & (y_pred == 0)).sum()
-    
-    precision = TP/(TP+FP)
-    recall = TP/(TP+FN)
-    
-    f1 = 2*(precision*recall)/(precision+recall)
+    FP = ((y_true == 0) & (y_pred == 1)).sum()
+    FN = ((y_true == 1) & (y_pred == 0)).sum()
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+
+    f1 = 2 * (precision * recall) / (precision + recall)
     return f1
 
 
-def F1_plot(F1_scores: np.ndarray, model: str)->None:
+def F1_plot(F1_scores: np.ndarray, model: str) -> None:
     """creates F1 score plot against bin size
 
     Args:
         F1_scores (np.ndarray): each row corresponds to one of 5 splits, each column is the bin size
         model (str): name of the model being analyzed
     """
-    bins = [5,10,15,20]
-    trace1 = go.Scatter(x=bins, y=F1_scores[:,0], mode='lines', name='split 1')
-    trace2 = go.Scatter(x=bins, y=F1_scores[:,1], mode='lines', name='split 2')
-    trace3 = go.Scatter(x=bins, y=F1_scores[:,2], mode='lines', name='split 3')
-    trace4 = go.Scatter(x=bins, y=F1_scores[:,3], mode='lines', name='split 4')
-    trace5 = go.Scatter(x=bins, y=F1_scores[:,4], mode='lines', name='split 5')
+    bins = [5, 10, 15, 20]
+    trace1 = go.Scatter(x=bins, y=F1_scores[:, 0], mode="lines", name="split 1")
+    trace2 = go.Scatter(x=bins, y=F1_scores[:, 1], mode="lines", name="split 2")
+    trace3 = go.Scatter(x=bins, y=F1_scores[:, 2], mode="lines", name="split 3")
+    trace4 = go.Scatter(x=bins, y=F1_scores[:, 3], mode="lines", name="split 4")
+    trace5 = go.Scatter(x=bins, y=F1_scores[:, 4], mode="lines", name="split 5")
     traces = [trace1, trace2, trace3, trace4, trace5]
-    
-    layout = go.Layout(title=f'F1 against bin size for {model}', xaxis=dict(title='bin size'), yaxis=dict(title='F1'))
-    
+
+    layout = go.Layout(
+        title=f"F1 against bin size for {model}",
+        xaxis=dict(title="bin size"),
+        yaxis=dict(title="F1"),
+    )
+
     fig = go.Figure(data=traces, layout=layout)
     fig.show()
     pass
--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-
 import decision_trees as dt
 import naive_bayes as nb
 import analysis
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-
+import random
 
 
 if __name__ == "__main__":
-    data = datasets.load_iris() # load dataset from sklearn
-    
-    iris_df = pd.DataFrame(data.data, columns=data.feature_names) # convert features to pandas
-    iris_df["target"] = (data.target == 0).astype(int) # set setosa as 1 and the other two as 0
-    
+    random.seed(50)
+    data = datasets.load_iris()  # load dataset from sklearn
+    # convert features to pandas
+    iris_df = pd.DataFrame(data.data, columns=data.feature_names)
+    # set setosa as 1 and the other two as 0
+    iris_df["target"] = (data.target == 0).astype(int)
 
+    bins = [5, 10, 15, 20]
+    accuracies = np.zeros(shape=(4, 5))
+    F1_score = np.zeros(shape=(4, 5))
+
+    states = [random.randint(10, 50) for __ in range(5)]
+    ######################################
+    # ---------- ID3 training ---------- #
+    ######################################
+
+    for i in range(5):
+        Xtrain, Xtest, Ytrain, Ytest = train_test_split(
+            iris_df.drop(["target"]),
+            iris_df["target"],
+            test_size=0.33,
+            random_state=states[i],
+        )
